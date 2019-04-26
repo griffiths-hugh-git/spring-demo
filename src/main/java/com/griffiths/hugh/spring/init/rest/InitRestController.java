040234f8377a2633af563574f15f6d3b7a03f709
Hugh <griffiths.hugh.git@gmail.com>
2019-04-26 11:20:13 +0100

    Controller

---
+++ src/main/java/com/griffiths/hugh/spring/init/rest/InitRestController.java
@@ -0,0 +1,13 @@
+package com.griffiths.hugh.spring.init.rest;
+
+import java.util.Date;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitRestController {
+	@RequestMapping("/hello")
+	public String hello(){
+		return new Date().toString();
+	}
+}
